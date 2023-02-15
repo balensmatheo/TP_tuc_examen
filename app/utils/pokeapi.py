4d270e68bdfd33d3d2255ff9915ca09dd204b863
LordTenebra <btermeau@gmail.com>
2023-02-15 10:23:18 +0100

    endpoint battle done

--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -28,7 +28,7 @@ def battle_pokemon(first_api_id, second_api_id):
     """
     premierPokemon = get_pokemon_data(first_api_id)
     secondPokemon = get_pokemon_data(second_api_id)
-    battle_result = 0
+    battle_result = battle_compare_stats(first_api_id, second_api_id)
     return premierPokemon if battle_result > 0 else secondPokemon if battle_result < 0 else {'winner': 'draw'}
 
 
@@ -36,3 +36,26 @@ def battle_compare_stats(first_pokemon_stats, second_pokemon_stats):
     """
         Compare given stat between two pokemons
     """
+    chalenger_1 = get_pokemon_stats(first_pokemon_stats)
+    chalenger_2 = get_pokemon_stats(second_pokemon_stats)
+    
+    chalenger_1_points = 0
+    chalenger_2_points = 0
+    count = 0
+    
+    for stat in chalenger_1:
+        if stat["base_stat"] > chalenger_2[count]["base_stat"]:
+            chalenger_1_points += 1
+            count += 1
+        elif stat["base_stat"] < chalenger_2[count]["base_stat"]:
+            chalenger_2_points += 1
+            count += 1
+        else:
+            count += 1
+            
+    if chalenger_1_points > chalenger_2_points:
+        return 1
+    elif chalenger_1_points < chalenger_2_points:
+        return -1
+    else:
+        return 0
