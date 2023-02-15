9bf4470c1847aace298089f163ee55dc517d857f
LordTenebra <btermeau@gmail.com>
2023-02-15 09:42:04 +0100

    endpoint battle

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter,  Depends
 from app import actions, schemas
 from app.utils.utils import get_db
+from app.utils.pokeapi import battle_pokemon
 
 router = APIRouter()
 
@@ -15,3 +16,9 @@ def get_pokemons(skip: int = 0, limit: int = 100, database: Session = Depends(ge
     """
     pokemons = actions.get_pokemons(database, skip=skip, limit=limit)
     return pokemons
+
+@router.get("/fight/{id_pokemon_1}/{id_pokemon_2}")
+def get_pokemons_fight(
+    id_pokemon_1: int, id_pokemon_2: int, database: Session = Depends(get_db)
+):
+    return battle_pokemon(id_pokemon_1, id_pokemon_2)
